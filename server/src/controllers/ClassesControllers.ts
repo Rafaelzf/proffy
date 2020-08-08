578f8c363271c70617d672c3b31df76d26a6542e
Rafaelzf <polako.rafa@gmail.com>
2020-08-08 20:44:31 -0300

    Join

--- server/src/controllers/ClassesControllers.ts
+++ server/src/controllers/ClassesControllers.ts
@@ -13,17 +13,24 @@ export default class ClassesContollers {
   async index(request: Request, response: Response) {
     const filters = request.query;
 
+    const subject = filters.subject as string;
+    const week_day = filters.week_day as string;
+    const time = filters.time as string;
+
     if (!filters.week_day || !filters.subject || !filters.time) {
       return response.status(400).json({
         error: "Erro ao filtrar as classes.",
       });
     }
 
-    const timeInMinutes = convertHoursToMinutes(filters.time as string);
+    const timeInMinutes = convertHoursToMinutes(time);
 
-    console.log(timeInMinutes);
+    const classes = await db("classes")
+      .where("classes.subject", "=", subject)
+      .join("users", "classes.user_id", "=", "users.id")
+      .select(["classes.*", "users.*"]);
 
-    response.send();
+    response.json(classes);
   }
 
   async create(request: Request, response: Response) {
