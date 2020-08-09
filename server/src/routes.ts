1db7be0f2335243c6c2fb4a074643204e9b1a86f
Rafaelzf <polako.rafa@gmail.com>
2020-08-08 22:14:54 -0300

    Finalização da API

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,11 +1,15 @@
 import express from "express";
-import ClassesContollers from "./controllers/classesControllers";
+import ClassesContollers from "./controllers/ClassesControllers";
+import ConectionsControllers from "./controllers/ConectionsControllers";
 
 const routes = express.Router();
 
 const classesControllers = new ClassesContollers();
+const classesConections = new ConectionsControllers();
 
 routes.post("/classes", classesControllers.create);
 routes.get("/classes", classesControllers.index);
+routes.post("/connections", classesConections.create);
+routes.get("/connections", classesConections.index);
 
 export default routes;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import routes from "./routes";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors());
 //converte o parametro em json para o express
 app.use(express.json());
 app.use(routes);
